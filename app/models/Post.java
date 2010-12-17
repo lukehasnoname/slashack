41d889ff03fb9727285b9111d883ecd7b157e7aa
Luke has no name <lukehasnoname@gmail.com>
2010-12-17 00:19:48 -0600

    Added models because I don't know how git works just yet.

---
+++ app/models/Post.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+import play.data.validation.*;
+
+@Entity
+public class Post extends Model{
+	
+	@Required
+	@ManyToOne
+	public User author;
+	@Required
+	public String title;
+	@Required
+	public String url;
+	@Lob
+	@MaxSize(100000)
+	public Date postdate;
+	@ManyToOne
+	public Post parent;
+	@ManyToOne
+	public Post root;
+	
+	public Post(User author, String title, String url, Post parent, Post root){
+		this.author = author;
+		this.title = title;
+		this.url = url;
+		this.postdate = new Date();
+		this.parent = parent;
+		this.root = root;
+	}
+}
---
+++ app/models/User.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+import play.data.validation.*;
+
+@Entity
+public class User extends Model {
+	
+	@Required
+	public String username;
+	@Required
+	public String password;
+	@Required
+	public String email;
+	public Date joindate;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	public List<Post> posts;
+	
+	public User(String username, String password, String email){
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.joindate = new Date();
+		
+	}
+}
